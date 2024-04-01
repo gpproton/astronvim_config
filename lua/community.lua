8b3f84e8561da9c8e54e1b59073be17d0ecc2507
Grayscale Pacifista <gpproton@live.com>
2024-04-01 18:42:31 +0100

    Update community.lua

--- lua/community.lua
+++ lua/community.lua
@@ -8,5 +8,38 @@ if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  { import = "astrocommunity.colorscheme.catppuccin" },
+  -- code runner
+  -- Overseer task runner & job management
+  { import = "astrocommunity.code-runner.overseer-nvim" },
+  -- switch between projects
+  { import = "astrocommunity.project.project-nvim" },
+  -- todo comments
+  { import = "astrocommunity.editing-support.todo-comments-nvim" },
+  {
+    "folke/todo-comments.nvim",
+    opts = {
+      sign_priority = 5, -- higher sign display priority
+    },
+  },
+  { import = "astrocommunity.pack.ansible" },
+  { import = "astrocommunity.pack.bash" },
+  { import = "astrocommunity.pack.cmake" },
+  { import = "astrocommunity.pack.go" },
+  { import = "astrocommunity.pack.python" },
+  { import = "astrocommunity.pack.cs" },
+  { import = "astrocommunity.pack.docker" },
+  { import = "astrocommunity.pack.html-css" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.yaml" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.angular" },
+  { import = "astrocommunity.pack.toml" },
+  -- { import = "astrocommunity.pack.php" },
+  -- { import = "astrocommunity.pack.proto" },
+  -- { import = "astrocommunity.pack.java" },
+  -- { import = "astrocommunity.pack.kotlin" },
+  -- { import = "astrocommunity.pack.ps1" },
 }
